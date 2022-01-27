7679eb7112cf1f4acdb71826764b3163d5badfe7
Ahtziri <Afalcon1209@gmail.com>
2022-01-26 19:40:06 -0800

    add js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "https://cdn.skypack.dev/react@17.0.1";
+import * as ReactDOM from "https://cdn.skypack.dev/react-dom@17.0.1";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const quoteData = [
+  {text: 'Heroes never die!', author: '- Mercy'},
+  {text: 'Justice rains from above!', author: '- Pharah'},
+  {text: 'DIE DIE DIE!', author: '- Reaper'},
+  {text: 'Its high noon', author: '- McCree'},
+  {text: 'Freeze! dont move!', author: '- Mei'},
+  {text: 'Nerf this!', author: '- D.va'},
+]
+
+const QuoteBox = ({quote, handleNewQuote}) => (
+
+    <div id="quote-box">
+    <p id='text'>{quote.text}</p>
+    <p id='author'>{quote.author}</p>
+    <div class='actions'>
+      <button 
+        id='new-quote'
+        class='button'
+        onClick={handleNewQuote}
         >
-          Learn React
-        </a>
-      </header>
+        New Quote
+      </button>
+      <a 
+        href="twitter.com/intent/tweet"
+        id="tweet-quote"
+        target="_blank"
+        >
+        Tweet
+      </a>
     </div>
-  );
+  </div>
+
+)
+
+const getRandomIndex = () => 
+  Math.round(Math.random() * ((quoteData.length-1) -0) + 0)
+
+
+const App = () => {
+  const [quote, setQuote] = React.useState(quoteData[getRandomIndex()])
+  const handleNewQuote = () => {
+    setQuote(quoteData[getRandomIndex()])
+  }
+  
+  return(
+  <div class="main">
+  <QuoteBox  quote={quote} handleNewQuote={handleNewQuote}/>
+</div>)
 }
 
-export default App;
+
+ReactDOM.render(<App />, document.querySelector('#app'))
